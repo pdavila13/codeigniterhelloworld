4bc601b51af5531fffce4a920ccf035b746027fe
Paolo Davila <pdavila@iesebre.com>
2014-10-01 21:57:50 +0200

    function parent

--- application/controllers/simpatic.php
+++ application/controllers/simpatic.php
@@ -2,9 +2,25 @@
 
 class Simpatic extends CI_Controller {
 
+	function __construct() {
+		parent::__construct();
+		$this->config->load('codeigniterhelloworld');
+	}
+	
 	public function index()
 	{
 		$this->load->view ('saluda');
+	}
+	
+	public function saluda($name){
+		$data = array();
+		
+		//$this->config->load('codeigniterhelloworld');
+		
+		$data['parem1'] = $name;
+		$data['param2'] = $this->config->item('academic_period');
+		
+		$this->load->view('saluda',$data);
 	}
 	
 }
